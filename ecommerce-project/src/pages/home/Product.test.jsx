ac29b45dfba9f3df60ecf095e2050fe631fcb461
pt-onwork <parthtomar1019@gmail.com>
2026-06-22 18:45:05 +0530

    use beforeach to remove duplications

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -1,4 +1,4 @@
-import { it, expect, describe, vi } from 'vitest'
+import { it, expect, describe, vi, beforeEach } from 'vitest'
 import { Product } from './Product'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event';
@@ -6,19 +6,27 @@ import axios from 'axios';
 
 vi.mock('axios');
 describe('Product Component', () => {
+
+  let product;
+  let loadCart;
+
+  beforeEach(()=>{
+    product= {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    rating: {
+      stars: 4.5,
+      count: 87
+    },
+    priceCents: 1090,
+    keywords: ["socks", "sports", "apparel"]
+  }
+  loadCart = vi.fn();
+
+  });
   it('displays the product details correctly', () => {
-    const product = {
-      id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-      name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-      rating: {
-        stars: 4.5,
-        count: 87
-      },
-      priceCents: 1090,
-      keywords: ["socks", "sports", "apparel"]
-    };
-    const loadCart = vi.fn();
+
     render(<Product product={product} loadCart={loadCart} />);
 
     expect(screen.getByText('Black and Gray Athletic Cotton Socks - 6 Pairs'))
@@ -42,29 +50,17 @@ describe('Product Component', () => {
   })
 
   it('adds a product to the cart', async () => {
-    const product = {
-      id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-      name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-      rating: {
-        stars: 4.5,
-        count: 87
-      },
-      priceCents: 1090,
-      keywords: ["socks", "sports", "apparel"]
-    };
-    const loadCart = vi.fn();
     render(<Product product={product} loadCart={loadCart} />);
 
     const user = userEvent.setup();
-    const addToCartButton=screen.getByTestId('add-to-cart-button')
+    const addToCartButton = screen.getByTestId('add-to-cart-button')
     await user.click(addToCartButton);
 
     expect(axios.post).toHaveBeenCalledWith(
-      '/api/cart-items',{
-        productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity:1
-      }
+      '/api/cart-items', {
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 1
+    }
     );
     expect(loadCart).toHaveBeenCalled();
 
